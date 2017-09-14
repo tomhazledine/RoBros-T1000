9875ecb6ef9cac16f33b74ebf75bd6084755ad84
Thomas Hazledine <tom.hazledine@headforwards.com>
2017-09-14 13:53:16 +0100

    distance whitespace fix

--- distance.py
+++ distance.py
@@ -19,35 +19,45 @@ try:
         # Set trigger to False (Low)
         GPIO.output(pinTrigger, False)
         # Allow module to settle
-time.sleep(0.5)
-# Send 10us pulse to trigger
-GPIO.output(pinTrigger, True)
-time.sleep(0.00001)
-GPIO.output(pinTrigger, False)
-# Start the timer
-StartTime = time.time()
-# The start time is reset until the Echo pin is taken high (==1)
-while GPIO.input(pinEcho)==0:
-    StartTime = time.time()
-# Stop when the Echo pin is no longer high - the end time
-while GPIO.input(pinEcho)==1:
-    StopTime = time.time()
-    # If the sensor is too close to an object, the Pi cannot
-    # see the echo quickly enough, so it has to detect that
-    # problem and say what has happened
-    if StopTime-StartTime >= 0.04:
-        print("Hold on there!  You're too close for me to see.")
-        StopTime = StartTime
-        break
-# Calculate pulse length
-ElapsedTime = StopTime - StartTime
-# Distance pulse travelled in that time is
+        time.sleep(0.5)
+
+        # Send 10us pulse to trigger
+        GPIO.output(pinTrigger, True)
+        time.sleep(0.00001)
+        GPIO.output(pinTrigger, False)
+
+        # Start the timer
+        StartTime = time.time()
+
+        # The start time is reset until the Echo pin is taken high (==1)
+        while GPIO.input(pinEcho)==0:
+            StartTime = time.time()
+
+        # Stop when the Echo pin is no longer high - the end time
+        while GPIO.input(pinEcho)==1:
+            StopTime = time.time()
+            # If the sensor is too close to an object, the Pi cannot
+            # see the echo quickly enough, so it has to detect that
+            # problem and say what has happened
+            if StopTime-StartTime >= 0.04:
+                print("Hold on there!  You're too close for me to see.")
+                StopTime = StartTime
+                break
+        
+        # Calculate pulse length
+        ElapsedTime = StopTime - StartTime
+
+        # Distance pulse travelled in that time is
         # time multiplied by the speed of sound (cm/s)
         Distance = ElapsedTime * 34326
+        
         # That was the distance there and back so halve the value
         Distance = Distance / 2
+        
         print("Distance: %.1f cm" % Distance)
+        
         time.sleep(0.5)
+        
 # If you press CTRL+C, cleanup and stop
 except KeyboardInterrupt:
     # Reset GPIO settings
